a1550a2767bb433d312eb64502f9235eb17e0558
cyasiaseeya <cyasiaseeya@gmail.com>
2025-02-01 01:57:50 +0900

    Update 막대기.py
    코드가 이상해서 재하가 고쳐줌. 담부턴 쉅 들으면서 코드 풀면 안되겠다.

--- Python/백준/Silver/1094. 막대기/막대기.py
+++ Python/백준/Silver/1094. 막대기/막대기.py
@@ -3,18 +3,15 @@
 o = 64
 ha = 0
 f = 0
-count = 0
-i = 10
+count = 1
 x = int(input())
-while(o + f > x):
-    if (o + f > x):
-        o /= 2
-        ha = o
+while(o + f != x):
+    o /= 2
+    ha = o
     if (o + f >= x):
         ha = 0
     else:
         f += ha
         count += 1
-f += o
-count += 1
+
 print(count)
